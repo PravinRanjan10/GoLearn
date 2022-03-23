4687d7c9a51a0c6a412a9dd85dd441326a159393
Pravin Ranjan <41313813+PravinRanjan10@users.noreply.github.com>
2022-03-23 22:32:14 +0530

    Update Int32Sort.go
    Sorting in array/slice

--- Sort/Int32Sort.go
+++ Sort/Int32Sort.go
@@ -7,7 +7,7 @@ func main()  {
 	var arr []int32
 	arr = []int32{0, 4 , 3, 8, 7, 9, 1, 10}
 	fmt.Println("arr before sort:", arr)
-	//sort.Ints(arr) // it sorts only []int
+	//sort.Ints(arr[:]) // it sorts only []int
 	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] }) // it sort other than []int
 	fmt.Println("arr after sort:", arr)
 }
