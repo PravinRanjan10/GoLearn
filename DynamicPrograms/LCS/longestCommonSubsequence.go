a939a301503af8f6f17f352abf7e6395a26f1269
Pravin Ranjan <pravinranjan10@gmail.com>
2025-04-27 15:35:31 +0530

    lcs update

--- DynamicPrograms/LCS/longestCommonSubsequence.go
+++ DynamicPrograms/LCS/longestCommonSubsequence.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// golbal variable for memoization
+var memo [][]int
+
 // recursive approach of finding LCS
 // Reference: https://www.youtube.com/watch?v=sSno9rV8Rhg
 func lcsRecursive(s1, s2 string, i, j int) int {
@@ -17,32 +20,21 @@ func lcsRecursive(s1, s2 string, i, j int) int {
 }
 
 // Memoization is also know as top down approach
-func lcsMemoization(s1, s2 string) int {
-	memo := make([][]int, len(s1)+1)
-	for i := range memo {
-		memo[i] = make([]int, len(s2)+1)
-		for j := range memo[i] {
-			memo[i][j] = -1
+func lcsMemoization(s1, s2 string, n, m int) int {
+	if memo[n][m] != -1 {
+		return memo[n][m]
+	}
+
+	if m == 0 || n == 0 {
+		memo[n][m] = 0
+	} else {
+		if s1[n-1] == s2[m-1] {
+			memo[n][m] = 1 + lcsMemoization(s1, s2, n-1, m-1)
+		} else {
+			memo[n][m] = max(lcsMemoization(s1, s2, n, m-1), lcsMemoization(s1, s2, n-1, m))
 		}
 	}
-	return lcsHelper(s1, s2, 0, 0, memo)
-}
-
-func lcsHelper(s1, s2 string, i, j int, memo [][]int) int {
-	if i == len(s1) || j == len(s2) {
-		return 0
-	}
-
-	if memo[i][j] != -1 {
-		return memo[i][j]
-	}
-
-	if s1[i] == s2[j] {
-		memo[i][j] = 1 + lcsHelper(s1, s2, i+1, j+1, memo)
-	} else {
-		memo[i][j] = max(lcsHelper(s1, s2, i+1, j, memo), lcsHelper(s1, s2, i, j+1, memo))
-	}
-	return memo[i][j]
+	return memo[n][m]
 }
 
 // Bottom-up, efficient solution
@@ -62,15 +54,11 @@ func lcsBottomUp(s1, s2 string) int {
 			if s1[i-1] == s2[j-1] {
 				dp[i][j] = dp[i-1][j-1] + 1
 			} else {
-				if dp[i-1][j] > dp[i][j-1] {
-					dp[i][j] = dp[i-1][j]
-				} else {
-					dp[i][j] = dp[i][j-1]
-				}
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 			}
 		}
 	}
-
+	fmt.Println("dpp==:", dp)
 	return dp[m][n]
 }
 
@@ -85,6 +73,14 @@ func main() {
 	s1 := "AGGTAB"
 	s2 := "GXTXAYB"
 	fmt.Println(lcsRecursive(s1, s2, len(s1), len(s2))) // outtput: 4, GTAB
-	fmt.Println(lcsMemoization(s1, s2))                 // outtput: 4, GTAB
+
+	memo = make([][]int, len(s1)+1)
+	for i := range memo {
+		memo[i] = make([]int, len(s2)+1)
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+	fmt.Println(lcsMemoization(s1, s2, len(s1), len(s2))) // outtput: 4, GTAB
 	fmt.Println(lcsBottomUp(s1, s2))
 }
