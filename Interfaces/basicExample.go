d6c8b5afac34f27daf0ff7d04613932af7dfcf9c
Pravin Ranjan <41313813+PravinRanjan10@users.noreply.github.com>
2025-08-11 07:44:39 +0530

    Update basicExample.go

--- Interfaces/basicExample.go
+++ Interfaces/basicExample.go
@@ -29,10 +29,27 @@ func (r rectangle) Area() int {
 
 func main() {
 	fmt.Println("I am in interfces!!")
+
+	//********* 1st Approach *********
 	var t tank
 	t = rectangle{5, 6, 10}
 	fmt.Println("rectangle area:", t.Area())
 
 	t = circle{5}
 	fmt.Println("Circle area:", t.Area())
+
+	// ******** 2nd Approach **********
+	c := circle{
+		radius: 4.5,
+	}
+	fmt.Println("rectangle area:", c.Area())
+
+	r := rectangle{
+		length: 4,
+		breath: 3,
+		height: 1,
+	}
+
+	fmt.Println("Circle area:", r.Area())
+
 }
