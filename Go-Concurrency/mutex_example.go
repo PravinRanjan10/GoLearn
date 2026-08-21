7d87242aaf512d83ec369fd76595d8f024c12ee7
Pravin Ranjan <41313813+PravinRanjan10@users.noreply.github.com>
2026-08-21 14:14:02 +0530

    Adding more example in go concurrency

--- Go-Concurrency/mutex_example.go
+++ Go-Concurrency/mutex_example.go
@@ -1,15 +1,16 @@
 /*
-Create 100 goroutines, and each goroutine should increment the counter 1,000
-times. So output should be: 100000
+Create 100 goroutines, and each goroutine should increment the counter 1,000 times. So output should be: 100000
 */
 package main
 
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var counter = 0
+var aCounter int64
 
 //var lock sync.Mutex
 
@@ -24,6 +25,15 @@ func Increment(lock *sync.Mutex, wg *sync.WaitGroup) {
 	lock.Unlock()
 }
 
+func IncrementUsingAtomic(wg *sync.WaitGroup) {
+	defer wg.Done()
+	i := 0
+	for i < 1000 {
+		atomic.AddInt64(&aCounter, 1)
+		i += 1
+	}
+}
+
 func main() {
 
 	i := 0
@@ -36,6 +46,13 @@ func main() {
 		i += 1
 	}
 
+	for i < 100 {
+		wg.Add(1)
+		go IncrementUsingAtomic(&wg) // here no need to pass locks
+		i += 1
+	}
+
 	wg.Wait()
 	fmt.Println("Counter==:", counter)
+	fmt.Println("Using atomic package Counter==:", aCounter)
 }
